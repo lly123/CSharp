9a72775894ae80cd52bac56544f3f6b7188b2a5f
Richard Li <lly123@gmail.com>
2012-05-06 19:19:46 +0800

    Add Mock Tests

--- CSharpDemo/MockClass.cs
+++ CSharpDemo/MockClass.cs
@@ -6,35 +6,43 @@ using Moq;
 
 namespace CSharpDemo
 {
-    public class MockBase
+    public interface IMockBase
     {
-        public int Add()
-        {
-            return 10;
-        }
+        void ShowMsg();
     }
 
-    public class MockDerived : MockBase
+    public class MockBase : IMockBase
     {
-        public new int Add()
+        public virtual void ShowMsg(){}
+        public virtual string ToLower(string s)
         {
-            return 20;
+            return s.ToLower();
+        }
+        public virtual int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
         }
     }
 
     class MockClass
     {
+        public void MockAnObject()
+        {
+            var mock = new Mock<MockBase>();
+            mock.Setup(m => m.ShowMsg()).Callback(() => Console.Out.WriteLine("ShowMsg is called."));
+            mock.Setup(m => m.ToLower(It.IsAny<string>())).Returns((string s) => s.ToLower());
+            mock.Setup(m => m.AddTwoNumbers(10, 8)).Returns((int a, int b) => a + b);
+
+            mock.Object.ShowMsg();
+            Console.Out.WriteLine("Convert 'ABC' to lower case: " + mock.Object.ToLower("ABC"));
+            Console.Out.WriteLine("10 + 8 = " + mock.Object.AddTwoNumbers(10, 8));
+
+            mock.VerifyAll();
+        }
+
         public void DoIt()
         {
-//            var mockObj = new Mock<MockBase>();
-//            mockObj.Setup(o => o.Add()).Returns(100);
-//
-//            Console.Out.WriteLine("Mock: " + mockObj.Object.Add());
-//
-//            mockObj.Verify();
-
-            MockDerived mb = new MockDerived();
-            Console.Out.WriteLine("Add: " + mb.Add());
+            MockAnObject();
         }
     }
 }
